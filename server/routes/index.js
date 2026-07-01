edd59addd29994148af24abcfc87cc6b28cdcfb7
Archana <sakthisiva8486@gmail.com>
2026-07-01 15:30:47 +0530

    feat: add initial API routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,3 +1,18 @@
 const express = require("express");
 const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Hi Home"
+    });
+});
+
+router.get("/about", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Hi About"
+    });
+});
+
 module.exports = router;
