89f2af141ba4026ccf47e0fbdad608dac1921cfc
Archana <sakthisiva8486@gmail.com>
2026-07-02 15:39:14 +0530

    refactor: remove notFound and applicationConstants modules

--- server/constants/applicationConstants.js
+++
@@ -1,6 +0,0 @@
-const application = {
-    PORT: process.env.PORT,
-    API_VERSION: "/api/v1"
-};
-
-module.exports = application;
--- server/middleware/notFound.js
+++
@@ -1,9 +0,0 @@
-module.exports = (req, res, next) => {
-
-    const error = new Error("Route Not Found");
-
-    error.statusCode = 404;
-
-    next(error);
-
-};
