3972b0a869d968901e72a12f26fba55786a91c6d
Archana <sakthisiva8486@gmail.com>
2026-07-02 15:33:00 +0530

    refactor(server): simplify application initialization

--- server/app.js
+++ server/app.js
@@ -1,11 +1,19 @@
 const express = require("express");
 
+const errorHandler = require("./middleware/errorHandler");
 const routes = require("./routes");
+const { httpError, StatusCodes } = require("./lib/httpError");
 
 const app = express();
 
 app.use(express.json());
-
 app.use("/api/v1", routes);
+app.use((req, res, next) => {
+    const error = new Error("Not Found");
+    error.statusCode = 404;
+    next(error);
+});
+
+app.use(errorHandler);
 
 module.exports = app;
