75392a043b55b8e0ff7fd2538b78203827de4b06
Archana <sakthisiva8486@gmail.com>
2026-07-16 12:26:41 +0530

    refactor(messages): update message exports

--- server/constants/messages/index.js
+++ server/constants/messages/index.js
@@ -4,11 +4,14 @@ const AUTH = require("./auth");
 const APPLICATION = require("./application");
 const DATABASE = require("./database");
 const RATE_LIMIT = require("./rateLimit");
+const USER = require("./user")
+
 module.exports = {
     SUCCESS,
     VALIDATION,
     AUTH,
     APPLICATION,
     DATABASE,
-    RATE_LIMIT
+    RATE_LIMIT,
+    USER
 };
