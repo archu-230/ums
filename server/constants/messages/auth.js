d0bd239cfb326d0f419fc9e0dde2e4a015ecffef
Archana <sakthisiva8486@gmail.com>
2026-07-16 12:26:15 +0530

    refactor(messages): update auth messages constants

--- server/constants/messages/auth.js
+++ server/constants/messages/auth.js
@@ -2,8 +2,12 @@ const AUTH = {
     EMAIL_ALREADY_REGISTERED: "Email already registered.",
     USER_NOT_FOUND: "User not found.",
     INVALID_CREDENTIALS: "Invalid email or password.",
-    INVALID_REFRESH_TOKEN: "Invalid Refresh Token",
+    INVALID_REFRESH_TOKEN: "Invalid refresh token.",
     UNAUTHORIZED: "Authentication required.",
+
+    AUTHORIZATION_TOKEN_REQUIRED: "Authorization token is required.",
+    ACCESS_TOKEN_MISSING: "Access token is missing.",
+    INVALID_ACCESS_TOKEN: "Invalid or expired access token.",
 };
 
 module.exports = AUTH;
