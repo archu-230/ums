ac96a3465e5f7a8cfb8eb0bb127fff279e62c4f7
Archana <sakthisiva8486@gmail.com>
2026-07-16 12:26:33 +0530

    feat(messages): add user messages constants

---
+++ server/constants/messages/user.js
@@ -0,0 +1,13 @@
+const USER = {
+    CREATED: "User created successfully.",
+    UPDATED: "User updated successfully.",
+    DELETED: "User deleted successfully.",
+
+    FOUND: "User fetched successfully.",
+    FOUND_ALL: "Users fetched successfully.",
+
+    NOT_FOUND: "User not found.",
+    EMAIL_EXISTS: "Email already exists.",
+};
+
+module.exports = USER;
