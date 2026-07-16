f88e84ed26deb3eaf09448fecf8bd125c7449f48
Archana <sakthisiva8486@gmail.com>
2026-07-16 12:26:50 +0530

    feat(auth): add bcrypt password hashing utility

--- server/utils/bcrypt.js
+++ server/utils/bcrypt.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 const APPLICATION = require("../constants/application");
 const hashPassword = async (password) => {
     return await bcrypt.hash(
