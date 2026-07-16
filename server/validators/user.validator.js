8d1b237faf7baefb44746a1ddf7cbb65d43c2fb9
Archana <sakthisiva8486@gmail.com>
2026-07-16 12:22:03 +0530

    feat(validation): add user validation rules

---
+++ server/validators/user.validator.js
@@ -0,0 +1,70 @@
+const { body, param } = require("express-validator");
+const VALIDATION = require("../constants/messages/validation");
+
+const createUserValidation = [
+    body("name")
+        .trim()
+        .notEmpty()
+        .withMessage(VALIDATION.NAME_REQUIRED),
+
+    body("email")
+        .trim()
+        .notEmpty()
+        .withMessage(VALIDATION.EMAIL_REQUIRED)
+        .isEmail()
+        .withMessage(VALIDATION.EMAIL_INVALID),
+
+    body("password")
+        .notEmpty()
+        .withMessage(VALIDATION.PASSWORD_REQUIRED)
+        .isLength({ min: 6 })
+        .withMessage(VALIDATION.PASSWORD_MIN_LENGTH),
+];
+
+const getUserByIdValidation = [
+    param("id")
+        .notEmpty()
+        .withMessage(VALIDATION.USER_ID_REQUIRED)
+        .isMongoId()
+        .withMessage(VALIDATION.USER_ID_INVALID),
+];
+
+const updateUserValidation = [
+    param("id")
+        .notEmpty()
+        .withMessage(VALIDATION.USER_ID_REQUIRED)
+        .isMongoId()
+        .withMessage(VALIDATION.USER_ID_INVALID),
+
+    body("name")
+        .optional()
+        .trim()
+        .notEmpty()
+        .withMessage(VALIDATION.NAME_EMPTY),
+
+    body("email")
+        .optional()
+        .trim()
+        .isEmail()
+        .withMessage(VALIDATION.EMAIL_INVALID),
+
+    body("password")
+        .optional()
+        .isLength({ min: 6 })
+        .withMessage(VALIDATION.PASSWORD_MIN_LENGTH),
+];
+
+const deleteUserValidation = [
+    param("id")
+        .notEmpty()
+        .withMessage(VALIDATION.USER_ID_REQUIRED)
+        .isMongoId()
+        .withMessage(VALIDATION.USER_ID_INVALID),
+];
+
+module.exports = {
+    createUserValidation,
+    getUserByIdValidation,
+    updateUserValidation,
+    deleteUserValidation,
+};
