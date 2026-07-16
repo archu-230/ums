1a81a8924e9708b7d9ebdbd1d283746bddbb06d9
Archana <sakthisiva8486@gmail.com>
2026-07-16 12:20:57 +0530

    feat(user): update user repository operations

--- server/repository/user-repository.js
+++ server/repository/user-repository.js
@@ -3,29 +3,82 @@ const User = require("../models/userModel");
 const create = async (user) => {
     return await User.create(user);
 };
-const findByEmail = async (email) => {
-    return await User.findOne({ email });
+
+const findAll = async (
+    filter = {},
+    options = {},
+) => {
+
+    const {
+        skip = 0,
+        limit = 10,
+        sort = {},
+    } = options;
+
+    const [users, totalRecords] =
+        await Promise.all([
+
+            User.find(filter)
+                .sort(sort)
+                .skip(skip)
+                .limit(limit),
+
+            User.countDocuments(filter),
+        ]);
+
+    return {
+
+        users,
+
+        totalRecords,
+    };
 };
 const findById = async (id) => {
-    return await User.findById(id);
-}
-const updateRefreshToken = async (
-    userId,
-    refreshToken
-) => {
+    return await User.findById(id)
+        .select("-password -refreshToken");
+};
+
+const findByEmail = async (email) => {
+    return await User.findOne({
+        email,
+    });
+};
+
+const update = async (id, user) => {
     return await User.findByIdAndUpdate(
-        userId, {
-        refreshToken,
-    },
+        id,
+        user,
+        {
+            new: true,
+            runValidators: true,
+        }
+    ).select("-password -refreshToken");
+};
+
+const remove = async (id) => {
+    return await User.findByIdAndDelete(
+        id
+    );
+};
+
+const updateRefreshToken = async (userId, refreshToken) => {
+    return await User.findByIdAndUpdate(
+        userId,
+        {
+            refreshToken,
+        },
         {
             new: true,
         }
     );
-}
+};
 
 module.exports = {
     create,
     findByEmail,
     findById,
-    updateRefreshToken
+    findAll,
+    update,
+    remove,
+    updateRefreshToken,
 };
