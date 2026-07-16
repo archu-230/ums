9e8a1784545694c548b97e9bf21bcb6ae335bfa0
Archana <sakthisiva8486@gmail.com>
2026-07-16 12:26:25 +0530

    refactor(messages): update validation messages

--- server/constants/messages/validation.js
+++ server/constants/messages/validation.js
@@ -1,9 +1,15 @@
 const VALIDATION = {
     NAME_REQUIRED: "Name is required.",
+    NAME_EMPTY: "Name cannot be empty.",
+
     EMAIL_REQUIRED: "Email is required.",
     EMAIL_INVALID: "Please enter a valid email address.",
+
     PASSWORD_REQUIRED: "Password is required.",
     PASSWORD_MIN_LENGTH: "Password must be at least 6 characters long.",
+
+    USER_ID_REQUIRED: "User ID is required.",
+    USER_ID_INVALID: "Invalid User ID.",
 };
 
 module.exports = VALIDATION;
