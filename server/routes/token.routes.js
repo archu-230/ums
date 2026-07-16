d268565b5dbe0d047dd81c6d332d146c07026cba
Archana <sakthisiva8486@gmail.com>
2026-07-16 12:19:52 +0530

    feat(auth): add refresh token route updates

--- server/routes/token.routes.js
+++ server/routes/token.routes.js
@@ -6,6 +6,38 @@ const validate = require("../middleware/validations");
 const tokenValidator = require("../validators/token.validator");
 const tokenController = require("../controllers/token.controller");
 
+/**
+ * @swagger
+ * tags:
+ *   name: Token
+ *   description: Token generation APIs
+ */
+
+
+/**
+ * @swagger
+ * /token/access-token:
+ *   post:
+ *     summary: Generate new access token
+ *     tags:
+ *       - Token
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               refreshToken:
+ *                 type: string
+ *                 example: eyJhbGciOiJIUzI1Ni...
+ *     responses:
+ *       200:
+ *         description: Access token generated
+ *       401:
+ *         description: Invalid token
+ */
+
 router.post(
     ROUTES.API.TOKEN.ACCESS_TOKEN,
     tokenValidator.access,
