3213fe584df42b366ce1d9dfd9ed9da4c3c58044
Archana <sakthisiva8486@gmail.com>
2026-07-19 18:53:15 +0530

    refactor(messages): add signup messages

---
+++ client/src/constants/messages/signup-messages.js
@@ -0,0 +1,13 @@
+const SIGNUP_MESSAGES = {
+    NAME_MIN: "Name must be atleast 3 characters",
+    NAME_MAX: "Name must be less than 20 characters",
+
+    EMAIL_INVALID: "Please enter a valid email",
+
+    PASSWORD_MIN: "Password must be at least 6 characters",
+    PASSWORD_MAX: "Password must be less than 15 characters",
+
+    SIGNUP_FAILED: "Failed to create account",
+};
+
+export default SIGNUP_MESSAGES;
