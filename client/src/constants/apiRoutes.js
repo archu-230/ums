eb124f23f6d2072e5eb4720897bec2f6ce763c91
Archana <sakthisiva8486@gmail.com>
2026-07-19 18:52:36 +0530

    refactor(constants): centralize API routes

---
+++ client/src/constants/apiRoutes.js
@@ -0,0 +1,12 @@
+const API_ROUTES = {
+    AUTH: {
+        SIGNUP: "/auth/signup",
+        LOGIN: "/auth/login",
+        REFRESH_TOKEN: "/auth/refresh-token",
+    },
+    USERS: {
+        BASE: "/users",
+    },
+};
+
+export default API_ROUTES;
