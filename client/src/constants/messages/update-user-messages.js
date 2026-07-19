e4be8e45febd52445da0b3de03c55ffcb66f935d
Archana <sakthisiva8486@gmail.com>
2026-07-19 18:53:20 +0530

    refactor(messages): add update user messages

---
+++ client/src/constants/messages/update-user-messages.js
@@ -0,0 +1,14 @@
+const UPDATE_USER_MESSAGES = {
+    NAME_MIN: "Name must be atleast 3 characters",
+    NAME_MAX: "Name must be less than 20 characters",
+
+    EMAIL_INVALID: "Please enter a valid email",
+
+    PASSWORD_MIN: "Password must be at least 6 characters",
+    PASSWORD_MAX: "Password must be less than 15 characters",
+
+    LOAD_FAILED: "Failed to load user",
+    UPDATE_FAILED: "Failed to update user",
+};
+
+export default UPDATE_USER_MESSAGES;
