27d38f4c8b1bedada5eb4a943e3067c4c90b6d1a
Archana <sakthisiva8486@gmail.com>
2026-07-19 18:53:07 +0530

    refactor(messages): add login messages

---
+++ client/src/constants/messages/login-messages.js
@@ -0,0 +1,9 @@
+const LOGIN_MESSAGES = {
+    EMAIL_INVALID: "Please enter a valid email",
+
+    PASSWORD_MIN: "Password must be at least 6 characters",
+
+    INVALID_CREDENTIALS: "Invalid email or password",
+};
+
+export default LOGIN_MESSAGES;
