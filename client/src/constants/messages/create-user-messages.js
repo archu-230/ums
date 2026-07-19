126aa00d563a769569ef3cedc30f984fe6d0728d
Archana <sakthisiva8486@gmail.com>
2026-07-19 18:53:00 +0530

    refactor(messages): add create user messages

---
+++ client/src/constants/messages/create-user-messages.js
@@ -0,0 +1,13 @@
+const CREATE_USER_MESSAGES = {
+    NAME_MIN: "Name must be atleast 3 characters",
+    NAME_MAX: "Name must be less than 20 characters",
+
+    EMAIL_INVALID: "Please enter a valid email",
+
+    PASSWORD_MIN: "Password must be at least 6 characters",
+    PASSWORD_MAX: "Password must be less than 15 characters",
+
+    CREATE_FAILED: "Failed to create user",
+};
+
+export default CREATE_USER_MESSAGES;
