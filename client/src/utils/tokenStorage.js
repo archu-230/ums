ad6b4aa6736a8ef827e306c3fd5c88111ecc1341
Archana <sakthisiva8486@gmail.com>
2026-07-21 11:02:59 +0530

    refactor(auth): update token storage utility

--- client/src/utils/tokenStorage.js
+++ client/src/utils/tokenStorage.js
@@ -1,25 +1,14 @@
 import STORAGE_KEYS from "../constants/storageKeys";
 
-export const getAccessToken = () => localStorage.getItem(STORAGE_KEYS.ACCESS_TOKEN);
-export const getRefreshToken = () => localStorage.getItem(STORAGE_KEYS.REFRESH_TOKEN);
-
-export const getUser = () => {
+export const getStoredUser = () => {
     const value = localStorage.getItem(STORAGE_KEYS.USER);
     return value ? JSON.parse(value) : null;
 };
 
-export const setAccessToken = (token) => {
-    localStorage.setItem(STORAGE_KEYS.ACCESS_TOKEN, token);
-};
-
-export const setAuthData = ({ user, accessToken, refreshToken }) => {
+export const setStoredUser = (user) => {
     localStorage.setItem(STORAGE_KEYS.USER, JSON.stringify(user));
-    localStorage.setItem(STORAGE_KEYS.ACCESS_TOKEN, accessToken);
-    localStorage.setItem(STORAGE_KEYS.REFRESH_TOKEN, refreshToken);
 };
 
-export const clearAuthStorage = () => {
+export const clearStoredUser = () => {
     localStorage.removeItem(STORAGE_KEYS.USER);
-    localStorage.removeItem(STORAGE_KEYS.ACCESS_TOKEN);
-    localStorage.removeItem(STORAGE_KEYS.REFRESH_TOKEN);
 };
