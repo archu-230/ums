5709575f76fb517ec05b55c7f2aaababd3a3841c
Archana <sakthisiva8486@gmail.com>
2026-07-21 11:03:06 +0530

    chore(server): update application configuration

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const helmet = require("helmet");
 const compression = require("compression");
+const cookieParser = require("cookie-parser");
 const swaggerUI = require("swagger-ui-express");
 const swaggerSpec = require("./config/swagger");
 
@@ -22,6 +23,8 @@ app.use(corsConfig);
 app.use(compression());
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.use(globalApiLimiter);
 
