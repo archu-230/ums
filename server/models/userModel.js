5be25567a98044feb4c23749d3123408ab203dcc
Archana <sakthisiva8486@gmail.com>
2026-07-21 11:05:12 +0530

    feat(user): extend user model for authentication

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -23,6 +23,10 @@ const userSchema = new mongoose.Schema(
             type: String,
             default: null,
         },
+        isBlocked: {
+            type: Boolean,
+            default: false,
+        },
     },
     {
         timestamps: true,
