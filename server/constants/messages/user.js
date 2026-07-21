008e3905883ffd6a031e297b905d16208fd73926
Archana <sakthisiva8486@gmail.com>
2026-07-21 11:04:46 +0530

    refactor(messages): update user messages

--- server/constants/messages/user.js
+++ server/constants/messages/user.js
@@ -2,6 +2,8 @@ const USER = {
     CREATED: "User created successfully.",
     UPDATED: "User updated successfully.",
     DELETED: "User deleted successfully.",
+    BLOCKED: "User blocked successfully.",
+    UNBLOCKED: "User unblocked successfully.",
 
     FOUND: "User fetched successfully.",
     FOUND_ALL: "Users fetched successfully.",
