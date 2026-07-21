b7e4da6325aa73aae315ced89df4ecd436dc25f4
Archana <sakthisiva8486@gmail.com>
2026-07-21 11:01:51 +0530

    refactor(messages): update API messages

--- client/src/constants/messages/api-messages.js
+++ client/src/constants/messages/api-messages.js
@@ -1,6 +1,7 @@
 const API_MESSAGES = {
     LOAD_USERS_FAILED: "Failed to load users",
     DELETE_USER_FAILED: "Failed to delete user",
+    TOGGLE_BLOCK_FAILED: "Failed to update user block status",
 };
 
 export default API_MESSAGES;
