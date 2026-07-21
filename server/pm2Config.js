fde8748e59843f17cb2afa2618e6e1a03c04af32
Archana <sakthisiva8486@gmail.com>
2026-07-21 16:14:33 +0530

    refactor: make pm2Config reusable

--- server/pm2Config.js
+++ server/pm2Config.js
@@ -1,29 +1,42 @@
-const pm2Config = {
+const APP_CONFIG = {
+    name: "ums-server",
+    script: "main.js",
+};
+
+const CLUSTER_CONFIG = {
+    exec_mode: "cluster",
+    instances: "max",
+};
+
+const WATCH_CONFIG = {
+    watch: false,
+    max_memory_restart: "300M",
+};
+
+const RESTART_CONFIG = {
+    autorestart: true,
+    max_restarts: 10,
+    min_uptime: "10s",
+    restart_delay: 3000,
+};
+
+const ENV_CONFIG = {
+    env: {
+        NODE_ENV: "development",
+    },
+    env_production: {
+        NODE_ENV: "production",
+    },
+};
+
+module.exports = {
     apps: [
         {
-            name: "ums-server",
-            script: "main.js",
-
-            exec_mode: "cluster",
-            instances: "max",
-
-            watch: false,
-            max_memory_restart: "300M",
-
-            env: {
-                NODE_ENV: "development",
-            },
-
-            env_production: {
-                NODE_ENV: "production",
-            },
-
-            autorestart: true,
-            max_restarts: 10,
-            min_uptime: "10s",
-            restart_delay: 3000,
+            ...APP_CONFIG,
+            ...CLUSTER_CONFIG,
+            ...WATCH_CONFIG,
+            ...RESTART_CONFIG,
+            ...ENV_CONFIG,
         },
     ],
 };
-
-module.exports = pm2Config;
