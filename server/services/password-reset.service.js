5d81bc7d200f51c8de47878b6c0e3516abe8d9a4
Archana <sakthisiva8486@gmail.com>
2026-07-21 15:44:11 +0530

    feat: enhance password reset service

--- server/services/password-reset.service.js
+++ server/services/password-reset.service.js
@@ -1,8 +1,12 @@
 const userService = require("./user.services");
 const emailService = require("./email.service");
-const { otpCache, resetTokenCache } = require("../config/cache");
+const { otpCache, usedResetTokenCache } = require("../config/cache");
 const { generateOtp } = require("../utils/otp");
-const { generateResetToken } = require("../utils/resetToken");
+const {
+    generateResetToken,
+    verifyResetToken,
+    RESET_TOKEN_TYPE,
+} = require("../utils/resetToken");
 const ENV = require("../constants/env");
 const { EMAIL_MESSAGES } = require("../constants/messages/email");
 const { NotFoundException, BadRequestException } = require("../lib/http-exceptions");
@@ -41,22 +45,41 @@ const requestResetLink = async (email) => {
     const user = await userService.findUserByEmail(email);
     if (!user) throw new NotFoundException(EMAIL_MESSAGES.USER_NOT_FOUND);
 
-    const token = generateResetToken();
-    resetTokenCache.set(token, email);
-
+    const token = generateResetToken(user.email);
     const resetLink = `${ENV.RESET_PASSWORD_URL}?token=${token}`;
-    await emailService.sendResetPasswordEmail({ email: user.email, name: user.name, resetLink });
+
+    await emailService.sendResetPasswordEmail({
+        email: user.email,
+        name: user.name,
+        resetLink,
+    });
 };
 
 const resetPasswordWithLink = async (token, newPassword) => {
-    const email = resetTokenCache.get(token);
-    if (!email) throw new BadRequestException(EMAIL_MESSAGES.PASSWORD_RESET_LINK_INVALID);
+    let decoded;
 
-    const user = await userService.findUserByEmail(email);
+    try {
+        decoded = verifyResetToken(token);
+    } catch (error) {
+        if (error.name === "TokenExpiredError") {
+            throw new BadRequestException(EMAIL_MESSAGES.PASSWORD_RESET_LINK_EXPIRED);
+        }
+        throw new BadRequestException(EMAIL_MESSAGES.PASSWORD_RESET_LINK_INVALID);
+    }
+
+    if (decoded.type !== RESET_TOKEN_TYPE) {
+        throw new BadRequestException(EMAIL_MESSAGES.PASSWORD_RESET_LINK_INVALID);
+    }
+
+    if (usedResetTokenCache.get(token)) {
+        throw new BadRequestException(EMAIL_MESSAGES.PASSWORD_RESET_LINK_INVALID);
+    }
+
+    const user = await userService.findUserByEmail(decoded.email);
     if (!user) throw new NotFoundException(EMAIL_MESSAGES.USER_NOT_FOUND);
 
     await userService.updatePassword(user._id, newPassword);
-    resetTokenCache.del(token);
+    usedResetTokenCache.set(token, true);
 };
 
 module.exports = {
