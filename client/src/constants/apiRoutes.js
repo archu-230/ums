2b58513207668ed43f602880b486fbb9e6559fce
Archana <sakthisiva8486@gmail.com>
2026-07-21 11:00:30 +0530

    refactor(constants): update API routes

--- client/src/constants/apiRoutes.js
+++ client/src/constants/apiRoutes.js
@@ -2,7 +2,16 @@ const API_ROUTES = {
     AUTH: {
         SIGNUP: "/auth/signup",
         LOGIN: "/auth/login",
+        LOGOUT: "/auth/logout",
         REFRESH_TOKEN: "/auth/refresh-token",
+        ME: "/auth/me",
+    },
+    PASSWORD: {
+        SEND_OTP: "/password/otp/send",
+        VERIFY_OTP: "/password/otp/verify",
+        RESET_WITH_OTP: "/password/otp/reset",
+        SEND_RESET_LINK: "/password/link/send",
+        RESET_WITH_LINK: "/password/link/reset",
     },
     USERS: {
         BASE: "/users",
