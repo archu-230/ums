ae1f84ea2650e1eea49bc9f34639662b123d60b5
Archana <sakthisiva8486@gmail.com>
2026-07-21 11:04:25 +0530

    refactor(messages): update success messages

--- server/constants/messages/success.js
+++ server/constants/messages/success.js
@@ -1,6 +1,8 @@
 const SUCCESS = {
     USER_REGISTERED: "User registered successfully.",
     LOGIN_SUCCESS: "Login successful.",
+    LOGOUT_SUCCESS: "Logout successful.",
+    PROFILE_FETCHED: "Profile fetched successfully.",
     REQUEST_SUCCESS: "Request completed successfully.",
     ACCESS_TOKEN_GENERATED: "Access token generated successfully.",
 
