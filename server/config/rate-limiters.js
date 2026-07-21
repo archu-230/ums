f4ca539c558f6e872fce6dcc4bf0adcf4ef9d278
Archana <sakthisiva8486@gmail.com>
2026-07-21 15:43:06 +0530

    feat: configure API rate limiters

--- server/config/rate-limiters.js
+++ server/config/rate-limiters.js
@@ -2,7 +2,7 @@ const rateLimit = require("express-rate-limit");
 const MESSAGES = require("../constants/messages");
 
 const globalApiLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
+    windowMs: 5 * 60 * 1000,
     max: 100,
     standardHeaders: true,
     legacyHeaders: false,
