137c04b09239cb7b3df7b60d9f3c62e88ff720c8
Archana <sakthisiva8486@gmail.com>
2026-07-21 11:05:37 +0530

    feat(routes): register password reset routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,8 +5,10 @@ const authRoutes = require("./auth.routes");
 const tokenRoutes = require("./token.routes");
 const ROUTES = require("../constants/routes");
 const userRoutes = require("./user.routes");
+const passwordResetRoutes = require("./password-reset.routes");
 
 router.use(ROUTES.API.AUTH.BASE_PATH, authRoutes);
 router.use(ROUTES.API.TOKEN.BASE_PATH, tokenRoutes);
 router.use(ROUTES.API.USER.BASE_PATH, userRoutes);
+router.use(ROUTES.API.PASSWORD.BASE_PATH, passwordResetRoutes);
 module.exports = router;
