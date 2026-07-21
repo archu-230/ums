61f6fe9a8d0319d34ea028588d8a00aec94ea683
Archana <sakthisiva8486@gmail.com>
2026-07-21 15:44:24 +0530

    feat: add JWT reset token utility

--- server/utils/resetToken.js
+++ server/utils/resetToken.js
@@ -1,9 +1,22 @@
-const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
+const ENV = require("../constants/env");
 
-const generateResetToken = () => {
-    return crypto.randomBytes(32).toString("hex");
+const RESET_TOKEN_TYPE = "password-reset";
+
+const generateResetToken = (email) => {
+    return jwt.sign(
+        { email, type: RESET_TOKEN_TYPE },
+        ENV.JWT_RESET_PASSWORD_SECRET,
+        { expiresIn: `${ENV.RESET_PASSWORD_EXPIRY_MINUTES}m` }
+    );
+};
+
+const verifyResetToken = (token) => {
+    return jwt.verify(token, ENV.JWT_RESET_PASSWORD_SECRET);
 };
 
 module.exports = {
     generateResetToken,
+    verifyResetToken,
+    RESET_TOKEN_TYPE,
 };
