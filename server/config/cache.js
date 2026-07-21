4bdbcfcfcfbabd86f4388f18894c91db86225ddc
Archana <sakthisiva8486@gmail.com>
2026-07-21 11:03:18 +0530

    refactor(cache): update cache configuration

--- server/config/cache.js
+++ server/config/cache.js
@@ -1,9 +1,20 @@
 const NodeCache = require("node-cache");
+const ENV = require("../constants/env");
 
-const nodeCache = new NodeCache({
-    stdTTL: 300,
-    checkperiod: 120,
+const otpCache = new NodeCache({
+    stdTTL: ENV.OTP_EXPIRY_MINUTES * 60,
+    checkperiod: 60,
     useClones: false,
 });
 
-module.exports = nodeCache;
+
+const resetTokenCache = new NodeCache({
+    stdTTL: ENV.RESET_PASSWORD_EXPIRY_MINUTES * 60,
+    checkperiod: 60,
+    useClones: false,
+});
+
+module.exports = {
+    otpCache,
+    resetTokenCache,
+};
